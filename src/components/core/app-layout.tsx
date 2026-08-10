0606e165c1a2270e0557ede088c4bea9fc211efa
Dhany Noor Alfian <dhanyalvian@gmail.com>
2026-08-10 10:42:08 +0700

    Sidebar Improvements - change color app title - fix size icon sidebar trigger

--- src/components/core/app-layout.tsx
+++ src/components/core/app-layout.tsx
@@ -34,7 +34,7 @@ const AppHeader = ({ breadcrumbItems }: AppHeaderProps) => {
   return (
     <header className="flex h-15 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-15 bg-main">
       <div className="flex items-center gap-2 px-4">
-        <SidebarTrigger className="-ml-1 size-9" />
+        <SidebarTrigger className="-ml-1" />
 
         <Separator
           orientation="vertical"
