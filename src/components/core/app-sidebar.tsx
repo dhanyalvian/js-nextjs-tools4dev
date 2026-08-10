8113b4092211fdd32b76274c3e603eecabbfb24d
Dhany Noor Alfian <dhanyalvian@gmail.com>
2026-08-10 10:42:08 +0700

    Sidebar Improvements - change color app title - fix size icon sidebar trigger

--- src/components/core/app-sidebar.tsx
+++ src/components/core/app-sidebar.tsx
@@ -28,7 +28,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton className="data-[slot=sidebar-menu-button]:p-1.5!" asChild>
               <Link href="/" aria-label="All Tools" className="text-primary">
                 <HugeiconsIcon icon={CodeXmlIcon} strokeWidth={2} className="size-5.5!" />
-                <span className="text-base font-semibold mt-0.5">Tools4Dev</span>
+                <span className="text-base text-foreground font-semibold mt-0.5">Tools4Dev</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
